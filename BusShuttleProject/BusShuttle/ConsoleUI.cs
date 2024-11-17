f557cb707648f4ed1959631b67ea1654b061e03a
Huseyin Ergin <mersinus@gmail.com>
2024-11-17 06:51:07 +0000

    Integrated into UI

--- BusShuttleProject/BusShuttle/ConsoleUI.cs
+++ BusShuttleProject/BusShuttle/ConsoleUI.cs
@@ -57,7 +57,7 @@ public class ConsoleUI
                     new SelectionPrompt<string>()
                         .Title("What do you want to do?")
                         .AddChoices(new[] {
-                            "add stop", "delete stop", "list all stops", "end"
+                            "show busiest stop", "add stop", "delete stop", "list all stops", "end"
                         }));
 
                 if(command=="add stop") {
@@ -73,7 +73,10 @@ public class ConsoleUI
                         table.AddRow(stop.Name);
                     }
                     AnsiConsole.Write(table);
-                } 
+                } else if(command=="show busiest stop") {
+                    var result = Reporter.FindBusiestStop(dataManager.PassengerData);
+                    Console.WriteLine("The busiest stop is: "+result.Name);
+                }
 
             } while (command != "end");
         }
